477c0c694245a54799fdfbce126ea29de37b86ae
loi <loi@hottab.net>
2016-03-02 14:42:46 +0700

    update

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ router.get('/', function(req, res) {
 
 var printingJob = function (data, callback) {
   //var htmlString = data.html;
+  console.log('pdfOptions', pdfOptions)
   var htmlString = "<h1>hello</h1>"
   data.copies = data.copies || 1;
   data.printers = data.printers || [];
